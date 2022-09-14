e537067a5f7145d9c327b65e1d218e50e0668a0e
BaXXeRFeeD <43886486+BaXXeRFeeD@users.noreply.github.com>
2022-09-14 21:14:43 +0300

    Fix lines, bytes,words. In main fix else if and files for and options for

--- main.cpp
+++ main.cpp
@@ -3,26 +3,26 @@
 #include <vector>
 #include <algorithm>
 
-int lines(std::ifstream &file) {
+long long lines(std::ifstream &file) {
     return std::count(std::istreambuf_iterator<char>(file),
                       std::istreambuf_iterator<char>(), '\n') + 1;
 }
 
-int bytes(std::ifstream &file) {
+size_t bytes(std::ifstream &file) {
     return file.tellg();
 }
 
-int words(std::string file_name, int count_lines) {
+int words(std::ifstream &file, long long countlines) {
     std::string str;
-    std::string File = "";
-    std::ifstream file(file_name);
-    for (int i = 0; i < count_lines; i++) {
+    std::string fileinstring = " ";
+    file.seekg(0);
+    for (int i = 0; i < countlines; i++) {
         std::getline(file, str);
-        File = File + " " + str;
+        fileinstring += (" " + str);
     }
     int count_words = 0;
-    for (int i = 0; i < File.size() - 1; i++) {
-        if (File[i] == ' ' && File[i + 1] != ' ')
+    for (int i = 0; i < fileinstring.size() - 1; i++) {
+        if (fileinstring[i] == ' ' && fileinstring[i + 1] != ' ')
             count_words++;
     }
     return count_words;
@@ -35,7 +35,7 @@ int main(int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
         std::string str = argv[i];
         if (str[str.size() - 1] == 't' && str[str.size() - 2] == 'x' &&
-            str[str.size() - 3] == 't')
+            str[str.size() - 3] == 't') //check file with .txt or not
             files.push_back(argv[i]);
         else
             options.push_back(argv[i]);
@@ -45,56 +45,48 @@ int main(int argc, char **argv) {
         lwc[1] = 1;
         lwc[2] = 1;
     }
-    for (int i = 0; i < options.size(); i++) {
+    for (int i = 0; i < options.size(); i++) { //options
         if (options[i][0] == options[i][1] && options[i][0] == '-') {
             if (options[i] == "--lines")
                 lwc[0] = 1;
+            else if (options[i] == "--words")
+                lwc[1] = 1;
+            else if (options[i] == "--bytes")
+                lwc[2] = 1;
             else {
-                if (options[i] == "--words")
-                    lwc[1] = 1;
-                else {
-                    if (options[i] == "--bytes")
-                        lwc[2] = 1;
-                    else {
-                        std::cout << "Error option";
-                        exit(1);
-                    }
-                }
+                std::cout << "Error option";
+                exit(1);
             }
-        }
-        for (int j = 1; j < options[i].size(); j++) {
-            if (options[i][j] == 'l')
-                lwc[0] = 1;
-            else {
-                if (options[i][j] == 'w')
+        } else {
+            for (int j = 1; j < options[i].size(); j++) {
+                if (options[i][j] == 'l')
+                    lwc[0] = 1;
+                else if (options[i][j] == 'w')
                     lwc[1] = 1;
+                else if (options[i][j] == 'c')
+                    lwc[2] = 1;
                 else {
-                    if (options[i][j] == 'c')
-                        lwc[2] = 1;
-                    else {
-                        std::cout << "Error option";
-                        exit(1);
-                    }
+                    std::cout << "Error option";
+                    exit(1);
                 }
             }
         }
     }
-    for (int i = 0; i < files.size(); i++) {
+    for (int i = 0; i < files.size(); i++) { // files
         std::ifstream file(files[i]);
         if (file.fail())
             std::cout << files[i] << " isn't present";
         else {
-            int count_lines = -1;
-            if (lwc[0] == 1 || lwc[1] == 1)
-                count_lines = lines(file);
-            if (lwc[0] == 1)
-                std::cout << count_lines << " ";
-            if (lwc[2] == 1)
-                std::cout << bytes(file) << " ";
-            if (lwc[1] == 1)
-                std::cout << words(files[i], count_lines) << " ";
+            long long countlines = lines(file);
+            size_t byte = bytes(file);
+            if (lwc[0] == 1) // lwc[0] - lines
+                std::cout << countlines << " ";
+            if (lwc[1] == 1) // lwc[1] - words
+                std::cout << words(file, countlines) << " ";
+            if (lwc[2] == 1) // lwc[2] - bytes
+                std::cout << byte << " ";
+            //if lines/words/bytes need, lwc[i] = 1
             std::cout << files[i] << "\n";
         }
     }
-    system("pause");
 }
