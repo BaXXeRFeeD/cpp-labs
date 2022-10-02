5c1be68f60038df6c333c576603d9aff7ccda388
BaXXeRFeeD <43886486+BaXXeRFeeD@users.noreply.github.com>
2022-10-02 20:18:55 +0300

    Changing the storage of digits of a number

--- lib/number.cpp
+++ lib/number.cpp
@@ -6,12 +6,11 @@ uint2022_t from_uint(uint32_t i) {
         uint2022_t.digits[t] = 0;
     }
     if (i == 0) {
-        uint2022_t.digits[0] = additionaldigit;
         uint2022_t.countdigits = 1;
     } else {
         int j = 0;
         while (i > 0) {
-            uint2022_t.digits[j] = i % additionaldigit + additionaldigit;
+            uint2022_t.digits[j] = i % additionaldigit;
             j++;
             i /= additionaldigit;
         }
@@ -42,12 +41,11 @@ uint2022_t from_string(const char *buff) {
                 uint2022.countdigits++;
             }
             for (int i = buffstring.size() - 1; i > buffstring.size() - 1 - (uint2022.countdigits - 1) * 9; i -= 9) {
-                uint2022.digits[count] = stoi(buffstring.substr(i - 8, 9)) + additionaldigit;
+                uint2022.digits[count] = stoi(buffstring.substr(i - 8, 9));
                 count++;
             }
             uint2022.digits[count] =
-                    stoi(buffstring.substr(0, buffstring.size() - 1 - (uint2022.countdigits - 1) * 9 + 1)) +
-                    additionaldigit;
+                    stoi(buffstring.substr(0, buffstring.size() - 1 - (uint2022.countdigits - 1) * 9 + 1));
             bool tr = false;
             for (int i = 0; i < buffstring.size() && uint2022.str; i++) {
                 if (buffstring[i] != MAXIMUM[i]) {
@@ -76,27 +74,18 @@ uint2022_t operator+(const uint2022_t &hs1, const uint2022_t &hs2) {
         for (int i = 0; i < digitssize; i++) {
             uint2022.digits[i] = 0;
         }
-        for (int i = fmin(lhs.countdigits, rhs.countdigits); i < fmax(lhs.countdigits, rhs.countdigits); i++) {
-            if (lhs.countdigits > rhs.countdigits) {
-                rhs.digits[i] = additionaldigit;
-            } else {
-                lhs.digits[i] = additionaldigit;
-            }
-        }
         for (int i = 0; i < fmax(lhs.countdigits, rhs.countdigits); i++) {
             uint2022.digits[i] =
-                    lhs.digits[i] - additionaldigit + rhs.digits[i] - additionaldigit + uint2022.digits[i];
+                    lhs.digits[i] + rhs.digits[i] + uint2022.digits[i];
             if (uint2022.digits[i] >= additionaldigit) {
                 uint2022.digits[i + 1] = 1;
-            } else {
-                uint2022.digits[i] += additionaldigit;
+                uint2022.digits[i] -= additionaldigit;
             }
         }
         if (uint2022.digits[fmax(lhs.countdigits, rhs.countdigits)] == 0) {
             uint2022.countdigits = fmax(lhs.countdigits, rhs.countdigits);
         } else {
             uint2022.countdigits = fmax(lhs.countdigits, rhs.countdigits) + 1;
-            uint2022.digits[uint2022.countdigits - 1] += additionaldigit;
         }
         if (!lhs.flag && !rhs.flag) {
             uint2022_t checker;
@@ -115,7 +104,7 @@ uint2022_t operator-(const uint2022_t &lhs, const uint2022_t &rhs) {
     uint2022.str = lhs.str * rhs.str;
     if (uint2022.str) {
         if (lhs == rhs) {
-            uint2022.digits[0] = additionaldigit;
+            uint2022.digits[0] = 0;
             uint2022.countdigits = 1;
         } else {
             uint2022_t lhs1 = lhs;
@@ -124,16 +113,13 @@ uint2022_t operator-(const uint2022_t &lhs, const uint2022_t &rhs) {
             for (int i = 0; i < digitssize; i++) {
                 uint2022.digits[i] = 0;
             }
-            for (int i = rhs1.countdigits; i < lhs1.countdigits; i++) {
-                rhs1.digits[i] = additionaldigit;
-            }
             for (int i = 0; i < lhs1.countdigits; i++) {
                 if (lhs1.digits[i] >= rhs1.digits[i] + uint2022.digits[i]) {
                     uint2022.digits[i] =
-                            (lhs1.digits[i] - uint2022.digits[i] - rhs1.digits[i]) + additionaldigit;
+                            (lhs1.digits[i] - uint2022.digits[i] - rhs1.digits[i]) ;
                 } else {
                     uint2022.digits[i] =
-                            2 * additionaldigit - rhs1.digits[i] - uint2022.digits[i] + lhs1.digits[i];
+                            additionaldigit - rhs1.digits[i] - uint2022.digits[i] + lhs1.digits[i];
                     uint2022.digits[i + 1] = 1;
                 }
             }
@@ -150,8 +136,8 @@ uint2022_t operator*(const uint2022_t &lhs, const uint2022_t &rhs) {
     uint2022.str = lhs.str * rhs.str;
     if (uint2022.str) {
         int count = 0;
-        if ((lhs.digits[0] == additionaldigit && lhs.countdigits == 1) ||
-            (rhs.digits[0] == additionaldigit && rhs.countdigits == 1)) {
+        if ((lhs.digits[0] == 0 && lhs.countdigits == 1) ||
+            (rhs.digits[0] == 0 && rhs.countdigits == 1)) {
             uint2022.digits[0] = 0;
             count = 1;
         } else {
@@ -169,7 +155,7 @@ uint2022_t operator*(const uint2022_t &lhs, const uint2022_t &rhs) {
                         uint2022.digits[i + j]++;
                         count++;
                     }
-                    mult = (uint64_t) (lhs.digits[i] - additionaldigit) * (uint64_t) (rhs.digits[j] - additionaldigit);
+                    mult = (uint64_t) lhs.digits[i] * (uint64_t) rhs.digits[j];
                     uint2022.digits[i + j] += mult % additionaldigit;
                     if (uint2022.digits[i + j] >= additionaldigit) {
                         if (uint2022.digits[i + j + 1] == -1) {
@@ -199,9 +185,6 @@ uint2022_t operator*(const uint2022_t &lhs, const uint2022_t &rhs) {
             }
         }
         if (uint2022.str) {
-            for (int i = 0; i < count; i++) {
-                uint2022.digits[i] += additionaldigit;
-            }
             uint2022.countdigits = count;
             if (!lhs.flag && !rhs.flag) {
                 uint2022_t checker;
@@ -246,6 +229,9 @@ bool operator!=(const uint2022_t &lhs, const uint2022_t &rhs) {
 
 std::ostream &operator<<(std::ostream &stream, uint2022_t value) {
     if (value.str) {
+        for(int i = 0; i < value.countdigits; i++){
+            value.digits[i] += additionaldigit;
+        }
         stream << value.digits[value.countdigits - 1] % additionaldigit;
         for (int i = value.countdigits - 2; i >= 0; i--) {
             std::string stringvalue = std::to_string(value.digits[i]);
